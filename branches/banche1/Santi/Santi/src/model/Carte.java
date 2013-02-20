13ffd13818f8adcea2c3aa31b70b9808092ded91
chris.dealm@gmail.com <chris.dealm@gmail.com@42355a2d-c173-b56c-77bf-436e0069d973>
2013-02-20 13:49:39 +0000

    nettoyage Branche 1

--- branches/banche1/Santi/Santi/src/model/Carte.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Carte {
-	private Position position;
-	private int nbMarqueurMax;
-	private int nbMarqueurActuel;
-	private boolean estDeserte;
-	private String type;
-}
--- branches/banche1/Santi/Santi/src/model/PositionSegment.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class PositionSegment extends Position {
-	private Position bout;
-	
-	public PositionSegment(int x, int y, boolean occupe) {
-		super(x, y, occupe);
-	}
-}
