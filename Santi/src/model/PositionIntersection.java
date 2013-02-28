03c0e2aecaa1cae5177536bfdfb14cd422b26888
anthony.d.dallo@gmail.com <anthony.d.dallo@gmail.com@42355a2d-c173-b56c-77bf-436e0069d973>
2013-02-28 14:17:24 +0000

    Placer source et canalsup done

--- Santi/src/model/PositionIntersection.java
+++ Santi/src/model/PositionIntersection.java
@@ -2,14 +2,30 @@ package model;
 
 public class PositionIntersection extends Position{
 	
-	public PositionIntersection() {
-		super();
+	public PositionIntersection(int x, int y) {
+		super(x,y);
 	}
 
 	public static PositionIntersection aleatoire(int niveau){
-		PositionIntersection pi = new PositionIntersection();
-		// TODO source alé Anthony
+		// TEST source alé Anthony
 		// l'aléatoire de la source (niveau 0 = normal, 1 = experimente) 
+		PositionIntersection pi;
+		if (niveau == 0){
+			//FIXME en normal on la met où ?
+			pi = new PositionIntersection(1,1);
+		}
+		else {
+			int x, y;
+			double random = Math.random()*8;
+			random = Math.floor(random);
+			Double d = new Double(random);
+			x = d.intValue();
+			random = Math.random()*6;
+			random = Math.floor(random);
+			d = new Double(random);
+			y = d.intValue();
+			pi = new PositionIntersection(x, y);
+		}
 		return pi;
 	}
 
