649d6e70ff16290a81d565474ed2d7c44ffb3c9c
anthony.d.dallo@gmail.com <anthony.d.dallo@gmail.com@42355a2d-c173-b56c-77bf-436e0069d973>
2013-03-03 14:25:09 +0000

    Position Alea Done

--- Santi/src/model/PositionIntersection.java
+++ Santi/src/model/PositionIntersection.java
@@ -1,30 +1,60 @@
 package model;
 
+import java.util.ArrayList;
+
 public class PositionIntersection extends Position{
+	private int niveau;
 	
-	public PositionIntersection(int x, int y) {
+	public PositionIntersection(int x, int y, int n) {
 		super(x,y);
+		this.niveau = n;
 	}
 
 	public static PositionIntersection aleatoire(int niveau){
 		// TEST source alé Anthony
-		// l'aléatoire de la source (niveau 0 = normal, 1 = experimente) 
-		PositionIntersection pi;
+		// l'aléatoire de la source (niveau 0 = normal, 1 = experimente)		
+		PositionIntersection pi = new PositionIntersection(-1,-1,-1);
+		int n = 2;
+		ArrayList<PositionIntersection> tabPos = new ArrayList<PositionIntersection>();
+		tabPos.add(new PositionIntersection (0,0,1));
+		tabPos.add(new PositionIntersection (0,2,1));
+		tabPos.add(new PositionIntersection (0,4,1));
+		tabPos.add(new PositionIntersection (0,6,1));
+		tabPos.add(new PositionIntersection (0,8,1));
+		tabPos.add(new PositionIntersection (2,0,1));
+		tabPos.add(new PositionIntersection (2,8,1));
+		tabPos.add(new PositionIntersection (4,0,1));
+		tabPos.add(new PositionIntersection (4,8,1));
+		tabPos.add(new PositionIntersection (6,0,1));
+		tabPos.add(new PositionIntersection (6,8,1));
+		tabPos.add(new PositionIntersection (2,2,0));
+		tabPos.add(new PositionIntersection (2,4,0));
+		tabPos.add(new PositionIntersection (2,6,0));
+		tabPos.add(new PositionIntersection (4,2,0));
+		tabPos.add(new PositionIntersection (4,4,0));
+		tabPos.add(new PositionIntersection (4,6,0));
+		tabPos.add(new PositionIntersection (6,2,0));
+		tabPos.add(new PositionIntersection (6,4,0));
+		tabPos.add(new PositionIntersection (6,8,0));
 		if (niveau == 0){
-			//FIXME en normal on la met où ?
-			pi = new PositionIntersection(1,1);
+			while (n != 0){
+				double rand = Math.random()*tabPos.size();
+				rand = Math.floor(rand);
+				Double d = new Double(rand);
+				int rdm = d.intValue();
+				n = tabPos.get(rdm).niveau;
+				pi = tabPos.get(rdm);
+			}
 		}
 		else {
-			int x, y;
-			double random = Math.random()*8;
-			random = Math.floor(random);
-			Double d = new Double(random);
-			x = d.intValue();
-			random = Math.random()*6;
-			random = Math.floor(random);
-			d = new Double(random);
-			y = d.intValue();
-			pi = new PositionIntersection(x, y);
+			while (n != 1){
+				double rand = Math.random()*tabPos.size();
+				rand = Math.floor(rand);
+				Double d = new Double(rand);
+				int rdm = d.intValue();
+				n =tabPos.get(rdm).niveau;
+				pi = tabPos.get(rdm);
+			}
 		}
 		return pi;
 	}
