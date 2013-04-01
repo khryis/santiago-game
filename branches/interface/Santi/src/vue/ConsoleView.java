b8321365ee04e36289a2d1dd6d4cabd0779a4a6b
chris.dealm@gmail.com <chris.dealm@gmail.com@42355a2d-c173-b56c-77bf-436e0069d973>
2013-04-01 11:57:23 +0000

    Quelques changement dans les vues

--- branches/interface/Santi/src/vue/ConsoleView.java
+++ branches/interface/Santi/src/vue/ConsoleView.java
@@ -3,7 +3,7 @@ package vue;
 import java.util.Observable;
 import java.util.Observer;
 
-public class VueConsole implements Observer {
+public class ConsoleView implements Observer {
     @Override
     public void update(Observable object, Object argument) {
         System.out.println((String)argument);
