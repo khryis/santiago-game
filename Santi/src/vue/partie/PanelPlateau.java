f273b1010f01c0f37f92fc238c9602ca5fc21aff
anthony.d.dallo@gmail.com <anthony.d.dallo@gmail.com@42355a2d-c173-b56c-77bf-436e0069d973>
2013-04-28 21:32:10 +0000

    Le plateau c'est bow

---
+++ Santi/src/vue/partie/PanelPlateau.java
@@ -0,0 +1,135 @@
+package vue.partie;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.Observable;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import vue.AbstractPanel;
+
+@SuppressWarnings("serial")
+public class PanelPlateau extends AbstractPanel{
+	private Image background;
+
+	public PanelPlateau(Container parent) {
+		super(parent);
+	}
+	
+	@Override
+	public void initComponent() {
+		super.initComponent();
+		setPreferredSize(new Dimension(800, 600));
+		setLayout(null);
+		JButton[] tabSegments = new JButton[31];
+		// Création des segments
+		for (int i = 0; i<31; i++) {
+			  JButton jb = new JButton();
+			  jb.setOpaque(false);
+			  jb.setContentAreaFilled(false);
+			  //jb.setBorderPainted(false); A DECOMMENTER POUR LAPERO
+			  tabSegments[i] = jb;
+			  add(jb);
+		}
+		int posX = 30;
+		int posY = 23;
+		// Positionnement des segements 
+		// Segments verticaux de 0 à 15 (total de 16)
+		for (int i = 0; i<16; i++){
+				if (i%4==0 && i!= 0){
+					posX=30;
+					posY+=180;
+				}
+				else 
+					if(i != 0)
+						posX += 183;
+				tabSegments[i].setBounds(posX, posY, 183, 8);
+				add(tabSegments[i]);
+		}
+		posX=30;
+		posY=31;
+		// Segements horizontaux de 16 à 30 (total de 15)
+		for (int i = 16; i<31; i++){
+			if (i == 21 || i == 26){
+				posX=30;
+				posY+=180;
+			}
+			else 
+				if(i != 16)
+					posX +=183;
+			tabSegments[i].setBounds(posX, posY, 8, 180);
+			add(tabSegments[i]);
+		}
+		
+		JButton[] tabCases = new JButton[48];
+		// Création des cases
+		for (int i = 0; i<48; i++) {
+			  JButton jb = new JButton();
+			  jb.setOpaque(false);
+			  jb.setContentAreaFilled(false);
+			  //jb.setBorderPainted(false); A DECOMMENTER POUR LAPERO
+			  tabCases[i] = jb;
+			  add(jb);
+		}
+		posX=38;
+		posY=31;
+		for (int i = 0; i<48; i++){
+			if (i%2==0 && i!=0 && i%6!=0 && i%8!=0 && i%16!=0){
+				posX+=97;
+			}
+			else {
+				if(i%8==0 && i!= 0 && i%16!=0){
+					posX=38;
+					posY+=87;
+				}
+				else{
+					if (i%16==0 && i!= 0){
+						posX=38;
+						posY+=96;
+					}
+					else 
+						if(i!= 0)
+							posX+=88;
+				}
+			}
+			tabCases[i].setBounds(posX, posY, 88, 88);
+		}
+		
+	    }
+
+	@Override
+	public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        try {
+            background = ImageIO.read(new File("img/plateau.jpg"));
+            // Pour l'image de fond
+            g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+	@Override
+	public void update(Observable arg0, Object arg1) {	
+	}
+
+	public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 600));
+
+        PanelPlateau pp = new PanelPlateau(frame);
+        frame.setContentPane(pp);
+        pp.initComponent();
+
+        frame.pack();
+        frame.setVisible(true);
+	}
+}
